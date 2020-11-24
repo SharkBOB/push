6d27c922095c7f72fbd7b86770b9a181196d7f06
Daniel <IGmfwang@163.com>
2020-11-24 14:42:41 +0800

    modify

--- The_Monthly_Update_Prices.py
+++ The_Monthly_Update_Prices.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-wb = pd.read_excel('RB20201102.xlsx')
+wb = pd.read_excel('RB20201124.xlsx')
 
 # wb.to_excel('newRB.xlsx')
 # print(wb.columns)
@@ -23,17 +23,19 @@ for i in GoodRetail:
 # Create the new excel
 # Using the merge methodology replace the VLookUp
 
-newdf = pd.DataFrame({'code': code, 'year': year, 'GoodRetail': GoodRetail, 'ComparePrices': ComparePrices})
+newdf = pd.DataFrame({'code': code,
+                      'year': year,
+                      'GoodRetail': GoodRetail,
+                      'ComparePrices': ComparePrices})
+
 JoinPricesRatio = newdf.merge(wb2, on='ComparePrices', how='left')
 JoinYearRatio = JoinPricesRatio.merge(wb3, on='year', how='left')
 
 # Merge the Prices Ratio and Year Ratio
 
-GoodWhosales = JoinYearRatio['GoodRetail']*(1-(JoinYearRatio['PricesRatio']*JoinYearRatio['YearRatio']))
+GoodWhosales = JoinYearRatio['GoodRetail']/(1+(JoinYearRatio['PricesRatio']*JoinYearRatio['YearRatio']))
 
 print(GoodWhosales)
-
-# Create the final excel
 finaldf=pd.DataFrame({'code': JoinYearRatio['code'],
                       'year': JoinYearRatio['year'],
                       'GoodRetail': JoinYearRatio['GoodRetail'],
@@ -41,6 +43,5 @@ finaldf=pd.DataFrame({'code': JoinYearRatio['code'],
                       'ComparePrices': JoinYearRatio['ComparePrices'],
                       'PricesRatio':JoinYearRatio['PricesRatio'],
                       'YearRatio':JoinYearRatio['YearRatio']})
-finaldf.to_excel('final.xlsx')
+finaldf.to_excel('final20201124.xlsx')
 print('OK')
-
