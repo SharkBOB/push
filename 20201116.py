b47c072271c48819d12f4ddb2c58ad6a5084d6e4
Daniel <IGmfwang@163.com>
2020-11-25 15:55:04 +0800

    modify

--- 20201116.py
+++ 20201116.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 # Get the dataset from new monthly upgrade
+
 wb = pd.read_excel('RB20201124.xlsx')
 
 # wb.to_excel('newRB.xlsx')
@@ -18,7 +19,9 @@ wb3 = pd.read_excel('NewMargin.xlsx', sheet_name='year margin using')
 prices = wb2['ComparePrices']
 PricesRatio = wb2['PricesRatio']
 YearRatio = wb3['YearRatio']
+
 # Loop the good retail ,get the closest coefficient to the compare prices.
+
 ComparePrices = []
 for i in GoodRetail:
     j = min(prices, key=lambda x: abs(x - i))
@@ -37,19 +40,18 @@ JoinYearRatio = JoinPricesRatio.merge(wb3, on='year', how='left')
 
 # Merge the Prices Ratio and Year Ratio
 
-GoodWhosales = JoinYearRatio['GoodRetail']/(1+(JoinYearRatio['PricesRatio']*JoinYearRatio['YearRatio']))
+GoodWhosales = JoinYearRatio['GoodRetail'] / (1 + (JoinYearRatio['PricesRatio'] * JoinYearRatio['YearRatio']))
 
 print(GoodWhosales)
 
 # Create the final excel to useing
-finaldf=pd.DataFrame({'code': JoinYearRatio['code'],
-                      'year': JoinYearRatio['year'],
-                      'GoodRetail': JoinYearRatio['GoodRetail'],
-                      'GoodWhosales': GoodWhosales,
-                      'ComparePrices': JoinYearRatio['ComparePrices'],
-                      'PricesRatio':JoinYearRatio['PricesRatio'],
-                      'YearRatio':JoinYearRatio['YearRatio']})
+
+finaldf = pd.DataFrame({'code': JoinYearRatio['code'],
+                        'year': JoinYearRatio['year'],
+                        'GoodRetail': JoinYearRatio['GoodRetail'],
+                        'GoodWhosales': GoodWhosales,
+                        'ComparePrices': JoinYearRatio['ComparePrices'],
+                        'PricesRatio': JoinYearRatio['PricesRatio'],
+                        'YearRatio': JoinYearRatio['YearRatio']})
 finaldf.to_excel('final20201124.xlsx')
 print('OK')
-
-
--- 20201121.py
+++ 20201121.py
@@ -3,9 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.optimize
 
-# print("new git commit")
-# print("I have some modify")
-# print("I want to update the git")
 
 import folium
 import pandas as pd
@@ -24,10 +21,19 @@ latitude = 37.77
 longitude = -122.42
 
 # Create map and display it
+
 san_map = folium.Map(location=[latitude, longitude], zoom_start=12)
 
 # Display the map of San Francisco
 san_map
 
 
+import numpy as np
+from sklearn import linear_model
 
+from sklearn import linear_model
+import scikitplot as skplt
+reg = linear_model.LinearRegression()
+reg.fit([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
+
+print(reg.coef_)
